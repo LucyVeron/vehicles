4a859641b0c2d07088bf9771039b7f267497e828
Lucy Veron <lucymveron@gmail.com>
2022-12-11 14:36:59 +0100

    atomic design structure, removed old code

--- src/utils/store.ts
+++ src/utils/store.ts
@@ -1,9 +1,15 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { createSlice } from '@reduxjs/toolkit';
+
+export const slice = createSlice({
+  name: 'name',
+  initialState: {},
+  reducers: {}
+});
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    reducer: slice.reducer,
   },
 });
 
