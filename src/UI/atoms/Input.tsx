902227d10ba17b61d346b260219327ee6f19ad86
Lucy Veron <lucymveron@gmail.com>
2022-12-18 16:14:55 +0100

    Removed unused Components

--- src/UI/atoms/Input.tsx
+++
@@ -1,7 +0,0 @@
-import { TextField } from "@mui/material";
-
-function Input(props: any) {
-  return <TextField className="Input" label={props.label} variant="outlined" />;
-}
-
-export default Input;
